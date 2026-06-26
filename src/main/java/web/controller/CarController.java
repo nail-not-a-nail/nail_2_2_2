a9e8aaf49168e407af30655d767a87d0450e8850
nail-not-a-nail <nolik-yanaul@hotmail.com>
2026-06-26 14:59:17 +0500

    interface

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 
@@ -14,12 +15,12 @@ import web.service.CarServiceImpl;
 public class CarController {
 
     @Autowired
-    private CarServiceImpl carServiceImpl;
+    private CarService carService;
 
     @GetMapping(value="")
     public String index ( @RequestParam(required = false, defaultValue = "5") int count,
                           ModelMap modelMap) {
-        modelMap.addAttribute("cars", carServiceImpl.index(count));
+        modelMap.addAttribute("cars", carService.index(count));
         return "car/cars";
     }
 }
